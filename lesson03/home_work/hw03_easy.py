f685271d183d8eb01272b5c167cb8deacd382243
AlexxNik <atnikolaev@inbox.ru>
2019-07-18 05:51:29 +0300

    AlexNik выполнил ДЗ easy и normal (hard не успел, планирую завершить когда будет время)

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -1,3 +1,5 @@
+__author__ = 'Alex Nik'
+
 # Задание-1:
 # Напишите функцию, округляющую полученное произвольное десятичное число
 # до кол-ва знаков (кол-во знаков передается вторым аргументом).
@@ -5,11 +7,32 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
+    if float(number) == 0:
+        return 'Округление невозможно: ' + number
+    check = str(number)[0 : (len(str(int(float(number) // 1))) + int(ndigits) + 2)]
+    if len(check[check.index('.') + 1 : ]) <= int(ndigits):
+        return 'Округление не требуется: ' + check
+    elif int(check[-1]) <= 4:
+        return float(check[0 : -1])
+    else:
+        if check[-2] == '.':
+            return float(check[0: -1]) + (1 / 10 ** int(ndigits))
+        elif int(check[-2]) == 9 and float(check) >= 0:
+            return float(check[0: -1]) + (1 / 10 ** int(ndigits))
+        elif int(check[-2]) == 9 and float(check) < 0:
+            return float(check[0: -1]) - (1 / 10 ** int(ndigits))
+        else:
+            return float(check[0 : -2] + str(int(check[-2]) + 1))
 
-
-print(my_round(2.1234567, 5))
-print(my_round(2.1999967, 5))
+number = ''
+ndigits = 0
+while number.isalpha() or int(ndigits) <= 0:
+    number = input('Введите число для округления: ')
+    ndigits = input('Введите желаемое количество знаков после запятой')
+    print('Введено неверное значение')
+print(my_round(number, ndigits))
+print(my_round(22.1234567, 5))
+print(my_round(-2.1999947, 5))
 print(my_round(2.9999967, 5))
 
 
@@ -20,9 +43,29 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    if len(str(ticket_number)) == 6:
+        left_numbers = list(str(ticket_number)[0 : 3])
+        right_numbers = list(str(ticket_number)[3 : ])
+        if sum(int (i) for i in left_numbers) == sum(int (i) for i in right_numbers):
+            return 'Счастливый билет!'
+        else:
+            return 'Обычный билет...'
+# ____another option____
+#        left_numbers = 0
+#        right_numbers = 0
+#        for i in str(ticket_number)[0 : 3]:
+#            left_numbers = left_numbers + int(i)
+#        for i in str(ticket_number)[3 : ]:
+#            right_numbers = right_numbers + int(i)
+#        if left_numbers == right_numbers:
+#            return 'Счастливый билет!'
+#        else:
+#            return 'Обычный билет...'
+    else:
+        return 'Данная программа проверяет только шестизначные номера билетов'
 
-
+user_ticket = input('\n\nВведите шестизначный номер билета: ')
+print(lucky_ticket(user_ticket))
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -1,18 +1,35 @@
+__author__ = 'Alex Nik'
+
 # Задание-1:
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
 def fibonacci(n, m):
-    pass
+    n = int(n)
+    m = int(m)
+    fibonacci_list = [1, 1]
+    for i in range(3, m + 1):
+        fibonacci_list.append(fibonacci_list[-1] + fibonacci_list[-2])
+    return fibonacci_list[n - 1 : m]
+
+user_N = 'f'
+user_M = 'f'
+while user_N.isalpha() or user_M.isalpha() or int(user_M) < 2:
+    user_N = input('Введите номер начального элемента ряда Фибоначчи: ')
+    user_M = input('Введите номер конечного элемента ряда Фибоначчи: ')
+print(fibonacci(user_N, user_M))
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
 # Для сортировки используйте любой алгоритм (например пузырьковый).
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
 
-
 def sort_to_max(origin_list):
-    pass
+    for i in range(len(origin_list)):
+        for j in range(len(origin_list)-i-1):
+            if origin_list[j] > origin_list[j + 1]:
+                origin_list[j], origin_list[j + 1] = origin_list[j + 1], origin_list[j]
+    return print(f'\nИсходный список: [2, 10, -12, 2.5, 20, -11, 4, 4, 0]\nСортировка завершена: {origin_list}')
 
 sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 
@@ -20,8 +37,77 @@ sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
+def func(positive):
+    if positive > 0:
+        return  1
+    else:
+        return  0
+
+def my_filt(func, obj):
+    result = []
+    for i in obj:
+        if func(i) == 1:
+            result.append(i)
+    return result
+
+my_list = [2, 10, -12, 2.5, 20, -11, 4, 4, 0]
+check = my_filt(func, my_list)
+print(f'\nИсходный список: {my_list}\nОтфильтрованы положительные значения: {check}')
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
 
+def maxim(user_list):
+    max = 0
+    max_index = 0
+    for i in user_list:
+        if i > max:
+            max = i
+            max_index = user_list.index(i)
+    return max_index
+
+def minim(user_list):
+    min = maxim(user_list)
+    min_index = 0
+    for i in user_list:
+        if i < min:
+            min = i
+            min_index = user_list.index(i)
+    return min_index
+
+def parallelograms_check(A1, A2, A3, A4):
+    x_center1 = 0
+    y_center1 = 0
+    x_center2 = 0
+    y_center2 = 0
+    par_set = list(zip(A1, A2, A3, A4))
+    par_list_x = par_set[0]
+    par_list_y = par_set[1]
+    x_center1 = (par_list_x[maxim(par_list_x)] + par_list_x[minim(par_list_x)]) / 2
+    y_center1 = (par_list_y[maxim(par_list_x)] + par_list_y[minim(par_list_x)]) / 2
+    par_set2 = [A1, A2, A3, A4]
+    par_set2.pop(maxim(par_list_x))
+    par_set2.pop(minim(par_list_x))
+    par_set2 = (list(par_set2[0]), list(par_set2[1]))
+    par_set2 = list(zip(par_set2[0], par_set2[1]))
+    par_list_x2 = par_set2[0]
+    par_list_y2 = par_set2[1]
+    x_center2 = (par_list_x2[0] + par_list_x2[1]) / 2
+    y_center2 = (par_list_y2[0] + par_list_y2[1]) / 2
+    if x_center1 == x_center2 and y_center1 == y_center2:
+        return print(f'Данная фигура является параллелограммом с координатами вершин: {A1}, {A2}, {A3}, {A4}')
+    else:
+        return print(f'Данная фигура с координатами вершин: {A1}, {A2}, {A3}, {A4} - не параллелограмм')
+
+A1 = [-14, 8]
+A2 = [-3, 11]
+A3 = [12, -4]
+A4 = [1, -7]
+parallelograms_check(A1, A2, A3, A4)
+
+A1 = [-14, 9]
+A2 = [-3, 11]
+A3 = [12, -4]
+A4 = [1, -7]
+parallelograms_check(A1, A2, A3, A4)
