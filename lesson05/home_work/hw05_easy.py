bb6f77c9d7d84eb46895fee59a960877bdd6a462
AlexxNik <atnikolaev@inbox.ru>
2019-07-24 04:59:05 +0300

    HW_05 AlexNik

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -1,10 +1,53 @@
+__author__ = 'Alex Nik'
+
 # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import os
+
+path = os.getcwd()
+print(f'The current directory: {path}')
+try:
+    folders_count = int(input('Set the number of folders:'))
+except ValueError:
+    print('An integer is required')
+else:
+    for i in range(1, folders_count + 1):
+        try:
+            os.mkdir(f'{path}/dir_{i}')
+        except OSError:
+            print(f'The folders is not created')
+        else:
+            print(f'The folder {path}/dir_{i} is created successfully')
+
+for dirs in os.listdir(path):
+    subfolder = os.path.join(path, dirs)
+    if os.path.isdir(subfolder):
+        os.rmdir(subfolder)
+        print(f'The empty folder {subfolder} is deleted')
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+count = 0
+for dirs in os.listdir(path):
+    subfolder = os.path.join(path, dirs)
+    if os.path.isdir(subfolder):
+        print(f'The current folder contains {subfolder}')
+        count += 1
+if count == 0:
+    print("The folders isn't in current directory")
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+from shutil import copy
+from inspect import getsourcefile
+
+def creat_file_copy(file_name):
+    return copy(getsourcefile(lambda: 0), f'{file_name}.py')
+
+name = input('Input new files name: ')
+creat_file_copy(name)
+print(f'The file {name} is created successfully')
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -1,3 +1,5 @@
+__author__ = 'Alex Nik'
+
 # Задача-1:
 # Напишите небольшую консольную утилиту,
 # позволяющую работать с папками текущей директории.
@@ -13,3 +15,88 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+import os
+import sys
+import shutil
+
+def print_help():
+    print('help - Getting help\n'
+          'open_dir - Go to folder\n'
+          'show_dirs_content - View the contents of the current folder\n'
+          'del_dir - Remote folder\n'
+          'make_dir - Create folder\n')
+    return
+
+def open_dir():
+    try:
+        user_dir = input('Enter the path to the folder:')
+        os.chdir(f'{user_dir}')
+    except OSError:
+        return print(f'The folders is not found\n')
+    else:
+        return print(f'The folder {user_dir} is active\n')
+
+def show_dirs_content():
+    path = os.getcwd()
+    print(f'The current directory: {path}')
+    count = 0
+    for dirs in os.listdir(path):
+        subfolder = os.path.join(path, dirs)
+        if os.path.isdir(subfolder):
+            print(f'The current folder contains {subfolder}')
+            count += 1
+    if count == 0:
+        print("There aren't folders in current directory\n")
+    return
+
+def del_dir():
+    path = os.getcwd()
+    print(f'The current directory: {path}')
+
+    try:
+        os.rmdir(path)
+    except PermissionError:
+        print(f'The empty folder {path} is not deleted') #не смог побороть эту ошибку...
+    else:
+        os.rmdir(path)
+        print(f'The empty folder {path} is deleted\n')
+
+def make_dir():
+    path = os.getcwd()
+    print(f'The current directory: {path}')
+    try:
+        user_dir = input('Enter the path to the new dir:')
+        if user_dir == '':
+            user_dir = path
+        os.mkdir(user_dir)
+    except OSError:
+        print(f'The folders is not created')
+    else:
+        print(f'The folder {user_dir} is created successfully')
+    return
+
+try:
+    dir_name = sys.argv[2]
+except IndexError:
+    dir_name = None
+
+try:
+    key = sys.argv[1]
+except IndexError:
+    key = None
+
+
+if key:
+    if do.get(key):
+        do[key]()
+    else:
+        print("ERROR: wrong key")
+        print("Enter key help for support")
+
+#Lesson_5
+do = {'print_help': print_help(),
+      'open_dir': open_dir(),
+      'show_dirs_content': show_dirs_content(),
+      'del_dir': del_dir(),
+      'make_dir': make_dir()}
